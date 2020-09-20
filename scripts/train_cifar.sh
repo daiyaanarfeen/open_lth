319ac94527520ca3610c12f0fc67b34508676161
root <root@h0.lottery-ticket.hearty.narwhal.pdl.cmu.edu>
2020-09-20 19:09:30 -0400

    high batch size script

---
+++ scripts/train_cifar.sh
@@ -0,0 +1,2 @@
+python open_lth.py lottery \
+	        --default_hparams cifar_resnet_20 --batch_size 512 --lr 0.1 --training_steps 7500it --milestone_steps 5000it,6250it --warmup_steps 5000it --levels 20 --num_workers 16 
